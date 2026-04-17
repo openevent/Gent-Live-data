471e5b4e1574724f6270434120b3d101c8971ac2
openevent <alani.faysel@gmail.com>
2026-04-17 20:12:50 +0200

    Initialize main.jsx with React and GhentOps component

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import GhentOps from './GhentOps.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GhentOps />
+  </React.StrictMode>
+)
